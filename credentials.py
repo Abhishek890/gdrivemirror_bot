0d35f1ecdbc41055865f1bc165ffbb823bc17e60
Abhishek890 <46078948+Abhishek890@users.noreply.github.com>
2019-09-25 10:48:41 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Atul-Kadian
 
+class Creds(object):
+        TOKEN = "881205921:AAF5QAv-2klD4oXb436E5EBvbn1agPzYzB8"
+        CLIENT_ID = "6969696969-gEtItFrOmGoOgLeClOuDcOnSoLe.apps.googleusercontent.com"
+        CLIENT_SECRET = "gEtItFrOmGoOgLeClOuDcOnSoLe"
